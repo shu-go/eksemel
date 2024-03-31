a19c4ac0a8eb3d5cbaf499aa52a093ad69353b67
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2024-03-31 16:59:13 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/shu-go/eksemel
+
+go 1.22.1
+
+require (
+	github.com/antchfx/xmlquery v1.3.18
+	github.com/go-xmlfmt/xmlfmt v1.1.2
+	github.com/shu-go/gli/v2 v2.0.1
+)
+
+require (
+	github.com/antchfx/xpath v1.2.5 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/shu-go/cliparser v0.2.2 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/antchfx/xmlquery"
+	"github.com/go-xmlfmt/xmlfmt"
+	"github.com/shu-go/gli/v2"
+)
+
+type globalCmd struct {
+	Replace replaceCmd
+	Delete  deleteCmd
+	Add     addCmd
+}
+
+type replaceCmd struct {
+	_ struct{} `help:"--xpath //* --value newvalue hoge.xml"`
+
+	XPath string `cli:"xpath" required:"true"`
+	Value string `cli:"value" required:"true"`
+}
+
+func (c replaceCmd) Run(args []string) error {
+	if len(args) == 0 {
+		return errors.New("input required")
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		return err
+	}
+	doc, err := xmlquery.Parse(f)
+	if err != nil {
+		return err
+	}
+
+	nodes, err := xmlquery.QueryAll(doc, c.XPath)
+	if err != nil {
+		return err
+	}
+	for _, n := range nodes {
+		if n.Type == xmlquery.AttributeNode {
+			n.Parent.SetAttr(n.Data, c.Value)
+		} else {
+			n.Data = c.Value
+		}
+	}
+
+	fmt.Println(strings.TrimSpace(xmlfmt.FormatXML(doc.OutputXML(true), "", "  ", true)))
+
+	return nil
+}
+
+type deleteCmd struct {
+	_ struct{} `help:"--xpath //* hoge.xml"`
+
+	XPath string `cli:"xpath" required:"true"`
+}
+
+func (c deleteCmd) Run(args []string) error {
+	if len(args) == 0 {
+		return errors.New("input required")
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		return err
+	}
+	doc, err := xmlquery.Parse(f)
+	if err != nil {
+		return err
+	}
+
+	nodes, err := xmlquery.QueryAll(doc, c.XPath)
+	if err != nil {
+		return err
+	}
+	for _, n := range nodes {
+		if n.Type == xmlquery.AttributeNode {
+			n.Parent.RemoveAttr(n.Data)
+		} else {
+			xmlquery.RemoveFromTree(n)
+		}
+	}
+
+	fmt.Println(strings.TrimSpace(xmlfmt.FormatXML(doc.OutputXML(true), "", "  ", true)))
+
+	return nil
+}
+
+type addCmd struct {
+	_ struct{} `help:"--xpath //* --name newnode --value newvalue hoge.xml"`
+
+	XPath   string `cli:"xpath" help:"parent" required:"true"`
+	Name    string `cli:"name" required:"true" help:"nodename, @attrname, #text, #cdata-section, #comment"`
+	Value   string `cli:"value" help:"if --name is a nodename, --value is its text"`
+	Sibling bool   `cli:"sibling" default:"false" help:"as a LAST sibling"`
+}
+
+func (c addCmd) Run(args []string) error {
+	if len(args) == 0 {
+		return errors.New("input required")
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		return err
+	}
+	doc, err := xmlquery.Parse(f)
+	if err != nil {
+		return err
+	}
+
+	nodes, err := xmlquery.QueryAll(doc, c.XPath)
+	if err != nil {
+		return err
+	}
+
+	addfunc := xmlquery.AddChild
+	if c.Sibling {
+		addfunc = xmlquery.AddSibling
+	}
+
+	for _, n := range nodes {
+		if strings.HasPrefix(c.Name, "@") {
+			xmlquery.AddAttr(n, c.Name[1:], c.Value)
+
+		} else if c.Name == "#text" {
+			addfunc(n, &xmlquery.Node{
+				Type: xmlquery.TextNode,
+				Data: c.Value,
+			})
+
+		} else if c.Name == "#cdata-section" {
+			nn := &xmlquery.Node{
+				Type: xmlquery.CharDataNode,
+				Data: c.Value,
+			}
+			addfunc(nn, &xmlquery.Node{
+				Type: xmlquery.TextNode,
+				Data: c.Value,
+			})
+			xmlquery.AddChild(n, nn)
+
+		} else if c.Name == "#comment" {
+			nn := &xmlquery.Node{
+				Type: xmlquery.CommentNode,
+				Data: c.Value,
+			}
+			//xmlquery.AddChild(nn, &xmlquery.Node{
+			//	Type: xmlquery.TextNode,
+			//	Data: c.Value,
+			//})
+			addfunc(n, nn)
+
+		} else {
+			nn := &xmlquery.Node{
+				Data: c.Name,
+			}
+			if c.Value != "" {
+				xmlquery.AddChild(nn, &xmlquery.Node{
+					Type: xmlquery.TextNode,
+					Data: c.Value,
+				})
+			}
+			addfunc(n, nn)
+
+		}
+	}
+
+	fmt.Println(strings.TrimSpace(xmlfmt.FormatXML(doc.OutputXML(true), "", "  ", true)))
+
+	return nil
+}
+
+// Version is app version
+var Version string
+
+func main() {
+	app := gli.NewWith(&globalCmd{})
+	app.Name = "eksemel"
+	app.Desc = "An XML file manipulator"
+	app.Version = Version
+	app.Usage = ``
+	app.Copyright = "(C) 2024 Shuhei Kubota"
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
+	}
+}
