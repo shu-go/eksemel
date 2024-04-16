9682b9d91237452e8c1ead24740c68215fb82270
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2024-04-16 19:15:24 +0900

    test: add delete/replace tests

---
+++ delete_test.go
@@ -0,0 +1,81 @@
+package main_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	main "github.com/shu-go/eksemel"
+	"github.com/shu-go/gotwant"
+)
+
+type deletetestdata struct {
+	input string
+	xpath string
+
+	indent int
+
+	err         error
+	out, errout string
+}
+
+func testdelete(t *testing.T, data []deletetestdata) {
+	t.Helper()
+
+	for i, d := range data {
+		in, out, errout := prepare(d.input)
+		err := main.Delete(
+			main.NewFakeCloseReader(in),
+			out,
+			errout,
+			d.xpath,
+			main.OutputConfig{Indent: strings.Repeat(" ", d.indent), EmptyElement: true},
+		)
+
+		seq := strconv.Itoa(i+1) + "/" + strconv.Itoa(len(data))
+
+		gotwant.TestError(t, err, d.err, gotwant.Desc(seq))
+		gotwant.Test(t, out.String(), d.out, gotwant.Desc(seq))
+		gotwant.Test(t, errout.String(), d.errout, gotwant.Desc(seq))
+	}
+}
+
+func TestDelete(t *testing.T) {
+	testdelete(t, []deletetestdata{
+		{
+			input: ``,
+			xpath: `/`,
+			out:   ``,
+		},
+		{
+			input: xmlpi + `<root><hoge/></root>`,
+			xpath: `/root/hoge`,
+			out:   xmlpi + `<root/>`,
+		},
+		{
+			input: xmlpi + `<root><hoge/><hoge/><hoge/></root>`,
+			xpath: `/root/hoge`,
+			out:   xmlpi + `<root/>`,
+		},
+		{
+			input: xmlpi + `<root><hoge/><hoge/><hoge/></root>`,
+			xpath: `/root/hoge[1]`,
+			out:   xmlpi + `<root><hoge/><hoge/></root>`,
+		},
+		{
+			input: xmlpi + `<root><hoge no="1"/><hoge no="2"/><hoge no="3"/></root>`,
+			xpath: `/root/hoge[1]`,
+			out:   xmlpi + `<root><hoge no="2"/><hoge no="3"/></root>`,
+		},
+		{
+			input: xmlpi + `<root><hoge no="1"/><hoge no="2"/><hoge no="3"/></root>`,
+			xpath: `/root/hoge[@no="2"]`,
+			out:   xmlpi + `<root><hoge no="1"/><hoge no="3"/></root>`,
+		},
+		{
+			input: xmlpi + `<root><hoge no="1"/><hoge no="2"/><fuga><hoge no="3"/></fuga></root>`,
+			xpath: `//hoge`,
+			out:   xmlpi + `<root><fuga/></root>`,
+		},
+	})
+}
---
+++ replace_test.go
@@ -0,0 +1,86 @@
+package main_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	main "github.com/shu-go/eksemel"
+	"github.com/shu-go/gotwant"
+)
+
+type replacetestdata struct {
+	input string
+	xpath string
+	value string
+	ennet string
+
+	indent int
+
+	err         error
+	out, errout string
+}
+
+func testreplace(t *testing.T, data []replacetestdata) {
+	t.Helper()
+
+	for i, d := range data {
+		in, out, errout := prepare(d.input)
+		err := main.Replace(
+			main.NewFakeCloseReader(in),
+			out,
+			errout,
+			d.xpath,
+			d.value,
+			d.ennet,
+			main.OutputConfig{Indent: strings.Repeat(" ", d.indent), EmptyElement: true},
+		)
+
+		seq := strconv.Itoa(i+1) + "/" + strconv.Itoa(len(data))
+
+		gotwant.TestError(t, err, d.err, gotwant.Desc(seq))
+		gotwant.Test(t, out.String(), d.out, gotwant.Desc(seq))
+		gotwant.Test(t, errout.String(), d.errout, gotwant.Desc(seq))
+	}
+}
+
+func TestReplace(t *testing.T) {
+	testreplace(t, []replacetestdata{
+		{
+			input: ``,
+			xpath: `/root`,
+			ennet: `root>hoge`,
+			out:   ``,
+		},
+		{
+			input: xmlpi + `<root><fuga/></root>`,
+			xpath: `/root`,
+			ennet: `root>hoge`,
+			out:   xmlpi + `<root><hoge/></root>`,
+		},
+		{
+			input: xmlpi + `<root><hoge/></root>`,
+			xpath: `/root`,
+			value: "loot",
+			out:   xmlpi + `<loot><hoge/></loot>`,
+		},
+		{
+			input: xmlpi + `<root><hoge/><hoge/><fuga><hoge/></fuga></root>`,
+			xpath: `/root/hoge`,
+			value: "moge",
+			out:   xmlpi + `<root><moge/><moge/><fuga><hoge/></fuga></root>`,
+		},
+		{
+			input: xmlpi + `<root><hoge/><hoge/><fuga><hoge/></fuga></root>`,
+			xpath: `//hoge`,
+			value: "moge",
+			out:   xmlpi + `<root><moge/><moge/><fuga><moge/></fuga></root>`,
+		},
+		{
+			input: xmlpi + `<root><!--comment--></root>`,
+			xpath: `/root/comment()`,
+			value: "komento",
+			out:   xmlpi + `<root><!--komento--></root>`,
+		},
+	})
+}
